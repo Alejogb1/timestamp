6adb9477915e7fe4b9bd4c91cf1e4d5a1d1ae315
Alejo <alejogarb25@gmail.com>
2021-03-24 18:12:08 -0300

    mvp of microservice 🎒

--- server.js
+++ server.js
@@ -1,36 +1,67 @@
-var express = require("express");
-var app = express()
+// server.js
+// where your node app starts
 
+// init project
+var express = require('express');
+var app = express();
 
-var cors = require("cors")
-app.use(cors({optionsSuccessStatus: 200}))
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
-app.use(express.static("public"))
+app.get("/api/timestamp", function(req, res) {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
+})
 
+app.get("/api/timestamp/:date_str?", function(req, res) {
+  const { date_str } = req.params;
 
-app.get("/api/timestamp/:date_str?", function (req, res) {
+  let date = new Date(date_str)
+  
+  console.log("DATE: ", date_str, typeof date_str)
+  
+  
 
-    const {date_str} = req.params;
-
-    let date = new Date(date_str);
-    
-
-    return res.json({
-        unix: date.getTime(),
-        utc: date.toUTCString()
+  if (date.toString() === "Invalid Date") {
+    date = new Date(parseInt(date_str))
+    console.log("INVALID DATE")
+  } 
+  
+  if(date.toString() === "Invalid Date") {
+      return res.json({
+          error: "Invalid Date"
     })
-
+  }else {
+    return res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    })
+  }
+  
 })
 
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/views/index.html")
-})
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
+
+// your first API endpoint... 
 app.get("/api/hello", function(req, res) {
-    res.sendFile({greeting: "Hello api"})
-})
+  res.json({ greeting: 'hello API' });
+});
 
-var listener = app.listen(8000, function () {
-    console.log("Your app is listening on port 8000")
-})
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
